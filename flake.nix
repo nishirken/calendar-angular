4a3c21ec0617beec5d86ccc7aedc032e6572a5a7
Dmitriy Skurikhin <dmitrii.sk@gmail.com>
2022-11-02 19:10:02 +0400

    initial commit

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        packageName = "calendar-angular";
+      in {
+        defaultPackage = self.packages.${system}.${packageName};
+
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodePackages.typescript-language-server
+            nodejs-16_x
+          ];
+        };
+      });
+}
+
